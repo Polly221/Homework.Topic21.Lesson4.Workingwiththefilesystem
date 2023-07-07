9e8aedbc66b81970017033472e790b4b771d2f9a
Polinka <Polinka@DESKTOP-MHBJDTO>
2023-07-07 13:11:36 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp24/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+
+Console.Write("Введите путь к файлу/папке: ");
+string path = Console.ReadLine()!;
+
+if (AskForConfirmation())
+{
+    try
+    {
+        if (File.Exists(path))
+        {
+            File.Delete(path);
+            Console.WriteLine("Файл успешно удален.");
+        }
+        else if (Directory.Exists(path))
+        {
+            Directory.Delete(path, true);
+            Console.WriteLine("Папка успешно удалена.");
+        }
+        else
+        {
+            Console.WriteLine("Файл/папка по указанному пути не существует.");
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Ошибка при удалении файла или папки: " + ex.Message);
+    }
+}
+else
+{
+    Console.WriteLine("Удаление отменено пользователем.");
+}
+
+Console.ReadLine();
+        
+
+        static bool AskForConfirmation()
+{
+    Console.Write("Вы уверены, что хотите удалить файл или папку? (да/нет): ");
+    string answer = Console.ReadLine()!;
+
+    return (answer == "да");
+}
